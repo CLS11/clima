f75622fd62334426cc5080fad8e7efd4b3bc170f
crystall.101600 <crystall.101600@gmail.com>
2025-02-22 16:59:57 +0000

    Contains all the constat values used in the app.

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const tempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100,
+);
+
+const messageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60,
+);
+
+const buttonTextStyle = TextStyle(
+  fontSize: 30,
+  fontFamily: 'Spartan MB',
+);
+
+const conditionTextStyle = TextStyle(
+  fontSize: 100,
+);
