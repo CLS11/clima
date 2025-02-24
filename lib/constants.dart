d43999f053c7ab1f7596d96e217b18f5aaa06e3e
crystall.101600 <crystall.101600@gmail.com>
2025-02-24 20:23:24 +0000

    Added textfield input decoration.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
-const tempTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 100,
-);
+const tempTextStyle = TextStyle(fontFamily: 'Spartan MB', fontSize: 100);
 
-const messageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 60,
-);
+const messageTextStyle = TextStyle(fontFamily: 'Spartan MB', fontSize: 60);
 
-const buttonTextStyle = TextStyle(
-  fontSize: 30,
-  fontFamily: 'Spartan MB',
-);
+const buttonTextStyle = TextStyle(fontSize: 30, fontFamily: 'Spartan MB');
 
-const conditionTextStyle = TextStyle(
-  fontSize: 100,
+const conditionTextStyle = TextStyle(fontSize: 100);
+
+const textFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city, 
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color:Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide.none,
+  ),
 );
