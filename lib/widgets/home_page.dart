e122135dad0bf06abaef51cc08d8e1e60831b064
crystall.101600 <crystall.101600@gmail.com>
2025-02-24 16:26:46 +0000

    Added the get data method with future.

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:myapp/components/location.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,6 +19,14 @@ class _HomePageState extends State<HomePage> {
   Future<void> getLocation() async {
     final location = Location();
     await location.getCurrentLocation();
+  }
+
+  Future<void> getData() async {
+    final url = Uri.parse(
+      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22',
+    );
+    final response = await http.get(url);
+    print(response);
   }
 
   @override
