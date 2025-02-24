7e96a07e92ce4237db3f943a92bf01bc8656de1f
crystall.101600 <crystall.101600@gmail.com>
2025-02-24 18:12:50 +0000

    Refactored the code.

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:myapp/components/location.dart';
+import 'package:myapp/components/networking.dart';
+
+const apiKey = '182045b56f2e3480a284defb58cb861b';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,23 +12,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double latitude;
+  late double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  Future<void> getLocation() async {
+  Future<void> getLocationData() async {
     final location = Location();
     await location.getCurrentLocation();
-  }
-
-  Future<void> getData() async {
-    final url = Uri.parse(
-      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22',
+    latitude = location.latitude!;
+    longitude = location.longitude!;
+    NetworkHelper networkHelper = NetworkHelper(
+      'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey',
     );
-    final response = await http.get(url);
-    print(response);
+    var weatherData = await networkHelper.getData();
+    // final temperature = decodedData['main']['temp'];
+    // final condition = decodedData['weather'][0]['id'];
+    // final cityName = decodedData['name'];
   }
 
   @override
