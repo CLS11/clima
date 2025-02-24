828281dc46d1056ea54a1997234c2a32823d9f4b
crystall.101600 <crystall.101600@gmail.com>
2025-02-24 18:15:05 +0000

    Network helper to store the api for the current latitude and longitude.

---
+++ lib/components/networking.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+  final String url;
+
+  Future<void> getData() async {
+    final url = Uri.parse('url');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
